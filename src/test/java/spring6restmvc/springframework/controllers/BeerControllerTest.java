4d4e2ee1dfe5d75402125b5e5ac4ef75af8616b6
Túlio Bitencourt <tulio.bf51@gmail.com>
2023-04-27 17:51:20 -0300

    Adding Unit Tests

---
+++ src/test/java/spring6restmvc/springframework/controllers/BeerControllerTest.java
@@ -0,0 +1,121 @@
+package spring6restmvc.springframework.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import spring6restmvc.springframework.controllers.BeerController;
+import spring6restmvc.springframework.model.Beer;
+import spring6restmvc.springframework.services.BeerService;
+import spring6restmvc.springframework.services.BeerServiceImpl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+    
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    BeerService beerService;
+
+    BeerServiceImpl beerServiceImpl;
+
+    @BeforeEach
+    void setUp() {
+        beerServiceImpl = new BeerServiceImpl();
+    }
+
+    @Test
+    void getBeerByid() throws Exception {
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+        given(beerService.getBeerById(testBeer.getUuid())).willReturn(testBeer);
+        mockMvc.perform(get("/api/v1/beer/" + testBeer.getUuid())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.uuid", is(testBeer.getUuid().toString())))
+                .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
+    }
+
+    @Test
+    void testListBeers() throws Exception {
+        List<Beer> testBeers = beerServiceImpl.listBeers();
+        given(beerService.listBeers()).willReturn(testBeers);
+        mockMvc.perform(get("/api/v1/beer").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)))
+                .andExpect(jsonPath("$[0].uuid", is(testBeers.get(0).getUuid().toString())));
+    }
+
+    @Test
+    void testCreateBeer() throws Exception {
+        Beer beer = beerServiceImpl.listBeers().get(0);
+        beer.setVersion(null);
+        beer.setUuid(null);
+
+        given(beerService.saveBeer(beer)).willReturn(beerServiceImpl.listBeers().get(2));
+        mockMvc.perform(post("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beer)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("location"));
+    }
+
+    @Test
+    void testUpdateBeer() throws Exception {
+        Beer beer = beerServiceImpl.listBeers().get(0);
+        mockMvc.perform(put("/api/v1/beer/" + beer.getUuid())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beer)))
+                .andExpect(status().isAccepted());
+        verify(beerService).updateById(beer.getUuid(), beer);
+    }
+
+    @Test
+    void testDeleteBeer() throws Exception {
+        Beer beer = beerServiceImpl.listBeers().get(0);
+        mockMvc.perform(delete("/api/v1/beer/" + beer.getUuid())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+        verify(beerService).deleteById(any(UUID.class));
+    }
+
+    @Test
+    void testPatchBeer() throws Exception{
+        Beer beer = beerServiceImpl.listBeers().get(0);
+
+        Map<String, Object> beerMap = new HashMap<>();
+        beerMap.put("beerName", "New Name");
+
+        mockMvc.perform(patch("/api/v1/beer/" + beer.getUuid())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beerMap)))
+                .andExpect(status().isNoContent());
+
+        verify(beerService).updateBeetPatchById(any(UUID.class), any(Beer.class));
+    }
+}
---
+++ src/test/java/spring6restmvc/springframework/controllers/CustomerControllerTest.java
@@ -0,0 +1,121 @@
+package spring6restmvc.springframework.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import spring6restmvc.springframework.model.Customer;
+import spring6restmvc.springframework.services.CustomerService;
+import spring6restmvc.springframework.services.CustomerServiceImpl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(CustomerController.class)
+class CustomerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    CustomerService customerService;
+
+    CustomerServiceImpl customerServiceImpl;
+
+    @BeforeEach
+    void setUp() {
+        customerServiceImpl = new CustomerServiceImpl();
+    }
+
+    @Test
+    void getAllCustomers() throws Exception {
+        List<Customer> customerList = customerServiceImpl.getAllCustomers();
+        given(customerService.getAllCustomers()).willReturn(customerList);
+        mockMvc.perform(get("/api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(2)))
+                .andExpect(jsonPath("$[0].uuid", is(customerList.get(0).getUuid().toString())));
+    }
+
+    @Test
+    void getCustomerById() throws Exception {
+        Customer testCustomer = customerServiceImpl.getAllCustomers().get(0);
+        given(customerService.getCustomerById(testCustomer.getUuid())).willReturn(testCustomer);
+        mockMvc.perform(get("/api/v1/customer/" + testCustomer.getUuid())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.uuid", is(testCustomer.getUuid().toString())));
+    }
+
+    @Test
+    void addCustomer() throws Exception {
+        Customer testCustomer = customerServiceImpl.getAllCustomers().get(0);
+        testCustomer.setVersion(null);
+        testCustomer.setUuid(null);
+
+        given(customerService.saveCustomer(testCustomer)).willReturn(customerServiceImpl.getAllCustomers().get(1));
+        mockMvc.perform(post("/api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(testCustomer)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("location"));
+    }
+
+    @Test
+    void updateCustomerById() throws Exception{
+        List<Customer> testCustomers = customerServiceImpl.getAllCustomers();
+
+        mockMvc.perform(put("/api/v1/customer/" + testCustomers.get(0).getUuid())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(testCustomers.get(1))))
+                .andExpect(status().isNoContent());
+        verify(customerService).updateCustomerById(testCustomers.get(0).getUuid(), testCustomers.get(1));
+    }
+
+    @Test
+    void deleteCustomerById() throws Exception {
+        Customer customer = customerServiceImpl.getAllCustomers().get(0);
+        mockMvc.perform(delete("/api/v1/customer/" + customer.getUuid())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+        verify(customerService).deleteByCustomerId(any(UUID.class));
+    }
+
+    @Test
+    void updateCustomerPatchById() throws Exception{
+        Customer customer = customerServiceImpl.getAllCustomers().get(0);
+
+        Map<String, Object> customerMap = new HashMap<>();
+        customerMap.put("customerName", "New Name");
+
+        mockMvc.perform(patch("/api/v1/customer/" + customer.getUuid())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customerMap)))
+                .andExpect(status().isNoContent());
+
+        verify(customerService).updateCustomerPatchById(any(UUID.class), any(Customer.class));
+    }
+}
